8e45eae9f3b4bc62d03df22574fa86169c1ca452
“Mrinmoyee <mrinmoyee.rm@gmail.com>
2022-11-20 16:25:13 -0800

    added colors and buttons and axis

--- barGraph.js
+++ barGraph.js
@@ -1,4 +1,4 @@
-var margin = {top: 10, right: 40, bottom: 10, left: 100};
+var margin = {top: 10, right: 40, bottom: 100, left: 100};
 var width = 860 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
 var svg = d3.select("body").append("svg")
@@ -32,11 +32,16 @@ d3.csv("barGraphData.csv", rowConverter, function(error, data) {
   xScale.domain(data.map(function(d){ return d.food; }));
   yScale.domain([0,d3.max(data, function(d) {return d.mass; })]);
   
+  var colorGrad = d3.scaleLinear().domain([1,2])
+  .range(["orange", "#db7093"])
+  
   // draw bars 
   svg.selectAll("rect") // creates an svg for rectangles
         .data(data) // load the data
         .enter()
         .append("rect") // shape of the bars
+        .transition().duration(500) // animation
+       .delay(function(d,i) {return i * 100;}) // animation timing delay
         .attr("x", function(d) { // returns the x values for the bars in the bar chart
             return xScale(d.food);
         })
@@ -46,7 +51,31 @@ d3.csv("barGraphData.csv", rowConverter, function(error, data) {
         .attr("width", xScale.bandwidth()) // width of each bar
         .attr("height", function(d) {
 			 return height- yScale(d.mass); // height of each bar
+        })
+        .attr("fill", function(d){return colorGrad(d.mass); 
         });
+  
+  
+  svg.append("g") 
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("dx", "-.8em") // styling of the x axis labels
+        .attr("dy", ".25em")
+        .style("text-anchor", "end")
+        .attr("font-size", "10px")
+        .attr("transform", "rotate(-60)");
+        
+//    
+//    // Draw yAxis and position the label
+//    svg.append("g")
+//        .attr("class", "y axis")
+//        .attr("transform", "translate(0," - height + ")")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px");
   
   
   // y axis label
@@ -57,13 +86,5 @@ d3.csv("barGraphData.csv", rowConverter, function(error, data) {
         .attr("x", -100)
         .attr("transform", "rotate(-90)") // rotates the label
         .text("Amount of ___ per 50 gallons of water"); // EDIT THIS LATER WITH TOOLTIP
-  
-  // x axis label
-  svg.append("text")
-        .attr("class", "x label")
-        .attr("text-anchor", "end")
-        .attr("y", 10) // position
-        .attr("x", 400)
-        .text("Food"); // EDIT THIS LATER WITH TOOLTIP
   
 });
