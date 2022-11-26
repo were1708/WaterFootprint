42ecb5e5344bd644fe982ac68dab345118ba0f42
“Mrinmoyee <mrinmoyee.rm@gmail.com>
2022-11-26 15:46:55 -0800

    added data for all buttons

--- barGraph.js
+++ barGraph.js
@@ -1,19 +1,56 @@
-var margin = {top: 10, right: 40, bottom: 100, left: 100};
-var width = 860 - margin.left - margin.right;
-var height = 600 - margin.top - margin.bottom;
+var margin = {top: 10, right: 40, bottom: 150, left: 100},
+    width = 760 - margin.left - margin.right, 
+    height = 500 - margin.top - margin.bottom; 
+    
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right) 
     .attr("height", height + margin.top + margin.bottom) 
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var xScale = d3.scaleBand().rangeRound([0, width]).padding(0.1);
 
-// range represents the biggest values on the left and the smallest values on the right
 var yScale = d3.scaleLinear().range([height, 0]);
 
-// Define X and Y AXIS
 var xAxis = d3.axisBottom(xScale);
 
-var yAxis = d3.axisLeft(yScale).ticks(5, "$5f");
+//
+//var xAxisDraw = svg.append("g")
+//  .attr("transform", "translate(0," + height + ")")
+
+//    svg.append("g") 
+//        .attr("class", "xaxis")
+//        .attr("transform", "translate(0," + height + ")")
+//        .call(xAxis)
+//        .selectAll("text")
+//        .attr("dx", "-.8em")
+//        .attr("dy", ".25em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px")
+//        .attr("transform", "rotate(-60)");
+
+var yAxis = d3.axisLeft(yScale);
+
+
+   var yAxisDraw   = svg.append("g")
+  .attr("class", "myYaxis")
+//       svg.append("g")
+//        .attr("class", "yaxis")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .attr("dx", "-0.1em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px");
+
+// y axis label
+  svg.append("text")
+        .attr("class", "ylabel")
+        .attr("text-anchor", "end")
+        .attr("y", -50) 
+        .attr("x", -100)
+        .attr("font-size", "12px")
+        .attr("transform", "rotate(-90)") 
+        .text("Amount of ___ per 50 gallons of water"); 
 
 function rowConverter(data) {
     return {
@@ -27,64 +64,324 @@ function rowConverter(data) {
 }
 
 d3.csv("barGraphData.csv", rowConverter, function(error, data) {
-//d3.csv("barGraphData.csv",rowConverter).then(function(data){
-  
-  xScale.domain(data.map(function(d){ return d.food; }));
-  yScale.domain([0,d3.max(data, function(d) {return d.mass; })]);
-  
-  var colorGrad = d3.scaleLinear().domain([1,2])
-  .range(["orange", "#db7093"])
-  
-  // draw bars 
-  svg.selectAll("rect") // creates an svg for rectangles
-        .data(data) // load the data
-        .enter()
-        .append("rect") // shape of the bars
-        .transition().duration(500) // animation
-       .delay(function(d,i) {return i * 100;}) // animation timing delay
-        .attr("x", function(d) { // returns the x values for the bars in the bar chart
-            return xScale(d.food);
-        })
-        .attr("y", function(d) { // returns the y values for the bars in the bar chart
-            return yScale(d.mass); 
-        })
-        .attr("width", xScale.bandwidth()) // width of each bar
-        .attr("height", function(d) {
-			 return height- yScale(d.mass); // height of each bar
-        })
-        .attr("fill", function(d){return colorGrad(d.mass); 
-        });
-  
-  
-  svg.append("g") 
-        .attr("class", "x axis")
+    xScale.domain(data.map(function(d){ return d.food; }));
+      svg.append("g") 
+        .attr("class", "xaxis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
         .selectAll("text")
-        .attr("dx", "-.8em") // styling of the x axis labels
+        .attr("dx", "-.8em")
         .attr("dy", ".25em")
         .style("text-anchor", "end")
         .attr("font-size", "10px")
         .attr("transform", "rotate(-60)");
+});
+
+var colorGrad = d3.scaleLinear().domain([1,2])
+  .range(["orange", "#db7093"])
+
+
+function update(id) {
+    d3.csv("barGraphData.csv", function(data) {
+        xScale.domain(data.map(function(d){ return d.Food; }));
         
-//    
-//    // Draw yAxis and position the label
-//    svg.append("g")
-//        .attr("class", "y axis")
-//        .attr("transform", "translate(0," - height + ")")
+//        xAxisDraw.transition().duration(1000).call(d3.axisBottom(xScale))
+//        xAxisDraw = svg.append("g")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px")
+//.attr("transform", "rotate(-60)")
+        
+        yScale.domain([0,d3.max(data, function(d) {return +d[id] })]);
+        
+        yAxisDraw.transition().duration(1000).call(d3.axisLeft(yScale));
+//        yAxisDraw =  svg.append("g")
+//          .transition().duration(1000)
+//        .attr("class", "yaxis")
 //        .call(yAxis)
 //        .selectAll("text")
+//        .attr("dx", "-0.1em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px") ;
+        
+        var u = svg.selectAll("rect")
+      .data(data)
+        
+        u
+      .enter()
+      .append("rect")
+      .merge(u)
+      .transition()
+      .duration(1000)
+        .attr("x", function(d) { return xScale(d.Food); })
+        .attr("y", function(d) { return yScale(d[id]); })
+        .attr("width", xScale.bandwidth())
+        .attr("height", function(d) { return height - yScale(d[id]); })
+        .attr("fill", "#69b3a2")
+    })
+}
+
+update('Mass')
+
+// AHHHHHHHHHH
+
+//d3.csv("barGraphData.csv", rowConverter, function(error, data) {
+//
+//  
+//  xScale.domain(data.map(function(d){ return d.food; }));
+//  yScale.domain([0,d3.max(data, function(d) {return d.mass; })]);
+//  
+//  // draw the x axis
+//  svg.append("g") 
+//        .attr("class", "xaxis")
+//        .attr("transform", "translate(0," + height + ")")
+//        .call(xAxis)
+//        .selectAll("text")
+//        .attr("dx", "-.8em")
+//        .attr("dy", ".25em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px")
+//        .attr("transform", "rotate(-60)");
+//    
+//
+//  
+//  // draw bars 
+//  svg.selectAll("rect") 
+//      .data(data) 
+//      .enter()
+//      .append("rect") 
+//      .transition().duration(500) 
+//      .delay(function(d,i) {return i * 100;}) 
+//      .attr("x", function(d) { return xScale(d.food);})
+//      .attr("y", function(d) { return yScale(d.mass);  })
+//      .attr("width", xScale.bandwidth()) 
+//      .attr("height", function(d) { return height- yScale(d.mass);  })
+//      .attr("fill", function(d){return colorGrad(d.mass); });
+//    
+//    d3.selectAll("button").on("click", function () {
+//        svg.selectAll("rect", "#yAxis").remove(); 
+//        
+//        yAxisDraw.remove();
+//        var val = d3.select(this).node().id; 
+//        
+////        update(data, val);
+//    })
+//  
+//});
+//
+//function update(data, val) {
+//    if (val == "fat") {
+//          yScale.domain([0,d3.max(data, function(d) {return d.fat; })]);
+//          yAxis = d3.axisLeft(yScale).ticks(13);
+//          yAxisDraw =  svg.append("g")
+//          .transition().duration(1000)
+//        .attr("class", "yaxis")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .attr("dx", "-0.1em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px") ;
+//  
+//          svg.selectAll("rect") 
+//              .data(data) 
+//              .enter()
+//              .append("rect") 
+//              .transition().duration(500) 
+//              .delay(function(d,i) {return i * 100;}) 
+//              .attr("x", function(d) { return xScale(d.food);})
+//              .attr("y", function(d) { return yScale(d.fat);  })
+//              .attr("width", xScale.bandwidth()) 
+//              .attr("height", function(d) { return height- yScale(d.fat);  })
+//              .attr("fill", function(d){return colorGrad(d.fat); });
+//      }
+//    
+//}
+//  
+
+
+
+// AHHHHHHHHH
+
+
+
+//  d3.selectAll("button").on("click", function () {
+//      console.log("hello")
+//      svg.selectAll("rect").remove(); 
+//      
+//      console.log(yAxis)
+//      yAxisDraw.remove();
+////      yScale.remove();
+////      yAxisDraw.selectAll("g.yaxis").remove();
+//      
+//     
+//      
+//      // action selects the id of the radio button pressed
+//      var val = d3.select(this).node().id; 
+////      console.log(typeof(val), val)
+//
+//      if (val == "fat") {
+//          yScale.domain([0,d3.max(data, function(d) {return d.fat; })]);
+//          yAxis = d3.axisLeft(yScale).ticks(13);
+//          yAxisDraw =  svg.append("g")
+//          .transition().duration(1000)
+//        .attr("class", "yaxis")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .attr("dx", "-0.1em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px") ;
+//  
+//          svg.selectAll("rect") 
+//              .data(data) 
+//              .enter()
+//              .append("rect") 
+//              .transition().duration(500) 
+//              .delay(function(d,i) {return i * 100;}) 
+//              .attr("x", function(d) { return xScale(d.food);})
+//              .attr("y", function(d) { return yScale(d.fat);  })
+//              .attr("width", xScale.bandwidth()) 
+//              .attr("height", function(d) { return height- yScale(d.fat);  })
+//              .attr("fill", function(d){return colorGrad(d.fat); });
+//      }
+//    })
+
+
+
+
+//d3.csv("barGraphData.csv", rowConverter, function(error, data) {
+//
+//  
+//  xScale.domain(data.map(function(d){ return d.food; }));
+//  yScale.domain([0,d3.max(data, function(d) {return d.mass; })]);
+//  
+//  // draw the x axis
+//  svg.append("g") 
+//        .attr("class", "xaxis")
+//        .attr("transform", "translate(0," + height + ")")
+//        .call(xAxis)
+//        .selectAll("text")
+//        .attr("dx", "-.8em")
+//        .attr("dy", ".25em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px")
+//        .attr("transform", "rotate(-60)");
+//    
+//    var yAxisDraw =  svg.append("g")
+//        .attr("class", "yaxis")
+//        .attr("id", "yAxis")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .attr("dx", "-0.1em")
 //        .style("text-anchor", "end")
 //        .attr("font-size", "10px");
-  
-  
-  // y axis label
-  svg.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("y", 20) // position
-        .attr("x", -100)
-        .attr("transform", "rotate(-90)") // rotates the label
-        .text("Amount of ___ per 50 gallons of water"); // EDIT THIS LATER WITH TOOLTIP
-  
-});
+//  
+//  // draw bars 
+//  svg.selectAll("rect") 
+//      .data(data) 
+//      .enter()
+//      .append("rect") 
+//      .transition().duration(500) 
+//      .delay(function(d,i) {return i * 100;}) 
+//      .attr("x", function(d) { return xScale(d.food);})
+//      .attr("y", function(d) { return yScale(d.mass);  })
+//      .attr("width", xScale.bandwidth()) 
+//      .attr("height", function(d) { return height- yScale(d.mass);  })
+//      .attr("fill", function(d){return colorGrad(d.mass); });        
+//  
+//  
+//  d3.selectAll("button").on("click", function () {
+//      console.log("hello")
+//      svg.selectAll("rect").remove(); 
+//      yAxisDraw.remove();
+////      yScale.remove();
+////      yAxisDraw.selectAll("g.yaxis").remove();
+//      
+//     
+//      
+//      // action selects the id of the radio button pressed
+//      val = d3.select(this).node().id; 
+////      console.log(typeof(val), val)
+//      if (val == "fat") {
+//          yScale.domain([0,d3.max(data, function(d) {return d.fat; })]);
+//          yAxis = d3.axisLeft(yScale).ticks(13);
+//          yAxisDraw =  svg.append("g")
+//          .transition().duration(1000)
+//        .attr("class", "yaxis")
+//        .call(yAxis)
+//        .selectAll("text")
+//        .attr("dx", "-0.1em")
+//        .style("text-anchor", "end")
+//        .attr("font-size", "10px");
+//  
+//          svg.selectAll("rect") 
+//              .data(data) 
+//              .enter()
+//              .append("rect") 
+//              .transition().duration(500) 
+//              .delay(function(d,i) {return i * 100;}) 
+//              .attr("x", function(d) { return xScale(d.food);})
+//              .attr("y", function(d) { return yScale(d.fat);  })
+//              .attr("width", xScale.bandwidth()) 
+//              .attr("height", function(d) { return height- yScale(d.fat);  })
+//              .attr("fill", function(d){return colorGrad(d.fat); });
+//      }
+////     else if (val == "fiber") {
+////          yScale.domain([0,d3.max(data, function(d) {return d.fiber; })]);
+////          svg.selectAll("rect") 
+////              .data(data) 
+////              .enter()
+////              .append("rect") 
+////              .transition().duration(500) 
+////              .delay(function(d,i) {return i * 100;}) 
+////              .attr("x", function(d) { return xScale(d.food);})
+////              .attr("y", function(d) { return yScale(d.fiber);  })
+////              .attr("width", xScale.bandwidth()) 
+////              .attr("height", function(d) { return height- yScale(d.fiber);  })
+////              .attr("fill", function(d){return colorGrad(d.fiber); });
+////      }
+////      else if (val == "cal") {
+////          yScale.domain([0,d3.max(data, function(d) {return d.cal; })]);
+////          svg.selectAll("rect") 
+////              .data(data) 
+////              .enter()
+////              .append("rect") 
+////              .transition().duration(500) 
+////              .delay(function(d,i) {return i * 100;}) 
+////              .attr("x", function(d) { return xScale(d.food);})
+////              .attr("y", function(d) { return yScale(d.cal);  })
+////              .attr("width", xScale.bandwidth()) 
+////              .attr("height", function(d) { return height- yScale(d.cal);  })
+////              .attr("fill", function(d){return colorGrad(d.cal); });
+////      }
+////      else if (val == "protein") {
+////          yScale.domain([0,d3.max(data, function(d) {return d.protein; })]);
+////          svg.selectAll("rect") 
+////              .data(data) 
+////              .enter()
+////              .append("rect") 
+////              .transition().duration(500) 
+////              .delay(function(d,i) {return i * 100;}) 
+////              .attr("x", function(d) { return xScale(d.food);})
+////              .attr("y", function(d) { return yScale(d.protein);  })
+////              .attr("width", xScale.bandwidth()) 
+////              .attr("height", function(d) { return height- yScale(d.protein);  })
+////              .attr("fill", function(d){return colorGrad(d.protein); });
+////      }
+////      else if (val == "mass") {
+////          yScale.domain([0,d3.max(data, function(d) {return d.mass; })]);
+////          svg.selectAll("rect") 
+////              .data(data) 
+////              .enter()
+////              .append("rect") 
+////              .transition().duration(500) 
+////              .delay(function(d,i) {return i * 100;}) 
+////              .attr("x", function(d) { return xScale(d.food);})
+////              .attr("y", function(d) { return yScale(d.mass);  })
+////              .attr("width", xScale.bandwidth()) 
+////              .attr("height", function(d) { return height- yScale(d.mass);  })
+////              .attr("fill", function(d){return colorGrad(d.mass); });
+////      }
+////        
+//      
+//    })
+//});
+
