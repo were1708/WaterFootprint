68aa9e83fc8eb7f23df1226b74c5446e4ee2427b
Mominah Khan <motkhan@ucsc.edu>
2022-12-06 14:05:46 -0800

    added grid layout

--- barGraph.js
+++ barGraph.js
@@ -8,7 +8,7 @@ var margin = {top: 10, right: 40, bottom: 150, left: 100},
     width = 760 - margin.left - margin.right, 
     height = 500 - margin.top - margin.bottom; 
     
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#svg3").append("svg")
 
     .attr("width", width + margin.left + margin.right) 
     .attr("height", height + margin.top + margin.bottom) 
